6bd65ef9f7bf938efc893b9fdcdad1e14181644c
roberto.junior <roberto.junior@gmail.com>
2025-06-11 23:59:14 -0300

    Ajustando testes de integração no ci

--- src/integration/java/com/orla/teste/FuncionarioIntegrationTest.java
+++ src/integration/java/com/orla/teste/FuncionarioIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -29,6 +31,13 @@ class FuncionarioIntegrationTest {
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
             .withDatabaseName("orla").withUsername("orla").withPassword("orla");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry reg) {
+        reg.add("spring.datasource.url",    postgres::getJdbcUrl);
+        reg.add("spring.datasource.username", postgres::getUsername);
+        reg.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Autowired
     private MockMvc mockMvc;
